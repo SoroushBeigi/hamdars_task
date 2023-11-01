08144f02b6d434432f5dfa45b3786d68eca09d26
Soroush Beigi <sbeygi712@gmail.com>
2023-11-01 11:11:28 +0330

    Replacing mock data with api

--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String baseUrl = 'https://app.taraz.org/api/qb/v4';
+  static const String baseUrl = 'https://app.tarazorg.ir/api/qb/v4';
   static const String lessonsEndpoint = '/test_unit';
 }
